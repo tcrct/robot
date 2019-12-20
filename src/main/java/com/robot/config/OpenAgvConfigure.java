b1cd695a9aeafbb3a82f97adfb3f8d351295d3da
tcrct@163.com <790731tcrct>
2019-12-20 18:50:18 +0800

    init submit

---
+++ src/main/java/com/robot/config/OpenAgvConfigure.java
@@ -0,0 +1,41 @@
+package com.robot.config;
+
+import cn.hutool.core.util.ClassUtil;
+import cn.hutool.log.Log;
+import cn.hutool.log.LogFactory;
+import io.netty.handler.codec.MessageToMessageDecoder;
+import io.netty.handler.codec.MessageToMessageEncoder;
+import org.opentcs.contrib.tcp.netty.ConnectionEventListener;
+
+import java.io.File;
+import java.net.URL;
+
+public class OpenAgvConfigure {
+
+    private static final Log logger = LogFactory.get();
+    private ConnectionEventListener connectionEventListener;
+    private MessageToMessageDecoder decoder;
+    private MessageToMessageEncoder encoder;
+
+    public OpenAgvConfigure() {
+        init();
+    }
+
+    private void init() {
+        String subDir = "config";
+        URL url = ClassUtil.getResourceURL(subDir);
+        String configPath = url.getPath();
+        File dir = new File(configPath);
+        configPath = dir.getParentFile().getAbsolutePath();
+        logger.info("OpenTcsConfigure Path: {}", configPath);
+//        System.setProperty("java.util.logging.config.file", configPath+File.separator+subDir+File.separator+"logging.config");
+        System.setProperty("java.security.policy", configPath +File.separator+subDir+File.separator+ "java.policy");
+        System.setProperty("opentcs.base", configPath);
+        System.setProperty("opentcs.home", ".");
+        System.setProperty("splash", configPath + File.separator+"bin"+File.separator+"splash-image.gif");
+        System.setProperty("file.encoding", "UTF-8");
+//        PropertyConfigurator.configure(configPath+File.separator+subDir+File.separator+"log4j.properties");
+        logger.warn("OpenAgvConfigure init success");
+
+    }
+}
